c22f8dcb130eb8657d3e9026e15ce77b7cc6de73
longwatermelon <longwatermelon1@gmail.com>
2024-05-19 18:19:56 -0700

    update

--- layer.h
+++ layer.h
@@ -23,9 +23,9 @@ struct Layer {
           relu(relu)
     {
         // random init W [-0.5,0.5]
-        for (int r = 0; r < W.rows(); ++r) {
-            for (int c = 0; c < W.cols(); ++c) {
-                W.atref(r, c) = (double)(rand() % 100) / 100. - .5;
+        for (int r=0; r<W.rows(); ++r) {
+            for (int c=0; c<W.cols(); ++c) {
+                W.atref(r,c)=(double)(rand()%100)/100.-.5;
             }
         }
     }
@@ -34,16 +34,16 @@ struct Layer {
         stringstream ss(s);
         int prev_n;
         ss >> n >> prev_n;
-        W = Matrix(n, prev_n);
-        b = Matrix(n, 1);
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < prev_n; ++j) {
-                ss >> W.atref(i, j);
+        W=Matrix(n, prev_n);
+        b=Matrix(n, 1);
+        for (int i=0; i<n; ++i) {
+            for (int j=0; j<prev_n; ++j) {
+                ss >> W.atref(i,j);
             }
         }
 
-        for (int i = 0; i < n; ++i) {
-            ss >> b.atref(i, 0);
+        for (int i=0; i<n; ++i) {
+            ss >> b.atref(i,0);
         }
 
         ss >> relu;
@@ -53,14 +53,14 @@ struct Layer {
         stringstream ss;
         // n, prev_n
         ss << n << ' ' << W.cols() << ' ';
-        for (int i = 0; i < W.rows(); ++i) {
-            for (int j = 0; j < W.cols(); ++j) {
-                ss << W.at(i, j) << ' ';
+        for (int i=0; i<W.rows(); ++i) {
+            for (int j=0; j<W.cols(); ++j) {
+                ss << W.at(i,j) << ' ';
             }
         }
 
-        for (int i = 0; i < b.rows(); ++i) {
-            ss << b.at(i, 0) << ' ';
+        for (int i=0; i<b.rows(); ++i) {
+            ss << b.at(i,0) << ' ';
         }
 
         ss << relu;
@@ -70,36 +70,36 @@ struct Layer {
 
 inline void forward_prop(Layer &l, Layer &prev)
 {
-    l.Z = l.W * prev.A;
-    for (int i = 0; i < l.Z.cols(); ++i) {
-        vector<double> &col = l.Z.get_col(i);
-        for (int j = 0; j < col.size(); ++j)
-            col[j] += l.b.at(j, 0);
+    l.Z=l.W*prev.A;
+    for (int i=0; i<l.Z.cols(); ++i) {
+        vector<double> &col=l.Z.get_col(i);
+        for (int j=0; j<col.size(); ++j)
+            col[j]+=l.b.at(j,0);
     }
 
-    l.A = l.Z;
+    l.A=l.Z;
     if (l.relu) {
-        for (int r = 0; r < l.A.rows(); ++r) {
-            for (int c = 0; c < l.A.cols(); ++c) {
-                l.A.atref(r, c) = max(0., l.Z.at(r, c));
+        for (int r=0; r<l.A.rows(); ++r) {
+            for (int c=0; c<l.A.cols(); ++c) {
+                l.A.atref(r,c)=max(0.,l.Z.at(r,c));
             }
         }
     }
 }
 
 inline void back_prop(Layer &l, const Layer &prev, const Layer *next,
-                      const Matrix &Y, Matrix &dW, Matrix &db, Loss lossfn = Loss::BinaryCrossentropy)
+                      const Matrix &Y, Matrix &dW, Matrix &db, Loss lossfn=Loss::BinaryCrossentropy)
 {
     if (!next) {
         // output layer
         switch (lossfn) {
-            case Loss::BinaryCrossentropy: l.dZ = l.A - Y; break;
+            case Loss::BinaryCrossentropy: l.dZ=l.A-Y; break;
             case Loss::Mse: {
-                l.dZ = (l.A - Y) * 2.;
+                l.dZ=(l.A-Y)*2.;
                 if (l.relu) {
-                    for (int r = 0; r < l.dZ.rows(); ++r) {
-                        for (int c = 0; c < l.dZ.cols(); ++c) {
-                            l.dZ.atref(r, c) = max(l.dZ.at(r, c), 0.);
+                    for (int r=0; r<l.dZ.rows(); ++r) {
+                        for (int c=0; c<l.dZ.cols(); ++c) {
+                            l.dZ.atref(r,c)=max(l.dZ.at(r,c),0.);
                         }
                     }
                 }
@@ -109,29 +109,29 @@ inline void back_prop(Layer &l, const Layer &prev, const Layer *next,
         // hidden layer
         Matrix gprime(l.Z.rows(), l.Z.cols());
         if (l.relu) {
-            for (int r = 0; r < gprime.rows(); ++r) {
-                for (int c = 0; c < gprime.cols(); ++c) {
-                    gprime.atref(r, c) = l.Z.at(r, c) > 0. ? 1. : 0.;
+            for (int r=0; r<gprime.rows(); ++r) {
+                for (int c=0; c<gprime.cols(); ++c) {
+                    gprime.atref(r,c)=l.Z.at(r,c)>0. ? 1. : 0.;
                 }
             }
         } else {
-            for (int r = 0; r < gprime.rows(); ++r) {
-                for (int c = 0; c < gprime.cols(); ++c) {
-                    gprime.atref(r, c) = 1.;
+            for (int r=0; r<gprime.rows(); ++r) {
+                for (int c=0; c<gprime.cols(); ++c) {
+                    gprime.atref(r,c)=1.;
                 }
             }
         }
 
-        l.dZ = (next->W.transpose() * next->dZ).element_mul(gprime);
+        l.dZ=(next->W.transpose()*next->dZ).element_mul(gprime);
     }
 
-    dW = l.dZ * prev.A.transpose() * (1. / Y.cols());
-    db = Matrix(l.dZ.rows(), 1);
-    for (int r = 0; r < l.dZ.rows(); ++r) {
-        db.atref(r, 0) = 0;
-        for (int c = 0; c < l.dZ.cols(); ++c) {
-            db.atref(r, 0) += l.dZ.at(r, c);
+    dW=l.dZ*prev.A.transpose()*(1./Y.cols());
+    db=Matrix(l.dZ.rows(), 1);
+    for (int r=0; r<l.dZ.rows(); ++r) {
+        db.atref(r,0)=0;
+        for (int c=0; c<l.dZ.cols(); ++c) {
+            db.atref(r,0)+=l.dZ.at(r,c);
         }
-        db.atref(r, 0) = db.at(r, 0) * (1. / Y.cols());
+        db.atref(r,0)=db.at(r,0)*(1./Y.cols());
     }
 }
--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include "layer.h"
-#include "util.h"
 #include <iostream>
 #include <fstream>
 #include <cstring>
@@ -8,80 +7,83 @@
 #define M 20
 
 double mse_cost(const Matrix &A, const Matrix &Y) {
-    double res = 0;
-    for (int i = 0; i < M; ++i) {
-        for (int j = 0; j < NF_OUT; ++j) {
-            res += pow(Y.at(j, i) - A.at(j, i), 2.);
+    double res=0;
+    for (int i=0; i<M; ++i) {
+        for (int j=0; j<NF_OUT; ++j) {
+            res+=pow(Y.at(j, i)-A.at(j, i), 2.);
         }
     }
-    res /= M;
+    res/=M;
 
     return res;
 }
 
 int main(int argc, char **argv) {
     srand(time(0));
-    bool train = argc == 2 && strcmp(argv[1], "train") == 0;
+    bool train=argc==2 && strcmp(argv[1], "train")==0;
 
+    ////////// DATA
     Matrix X(NF, M);
     Matrix Y(NF_OUT, M);
-    for (int i = 0; i < M; ++i) {
-        X.atref(0, i) = (double)i / M;
-        X.atref(1, i) = (double)i*i / (M*M);
-        X.atref(2, i) = (double)i*i*i / (M*M*M);
-        Y.atref(0, i) = (double)(i + i*i + i*i*i);
+    for (int i=0; i<M; ++i) {
+        X.atref(0, i)=(double)i / M;
+        X.atref(1, i)=(double)i*i / (M*M);
+        X.atref(2, i)=(double)i*i*i / (M*M*M);
+        Y.atref(0, i)=(double)(i + i*i + i*i*i);
     }
 
     vector<Layer> layers;
     if (train) {
-        layers = {
+        ////////// TRAIN
+        layers={
             Layer(NF, 1, false),
             Layer(10, NF, false),
             Layer(NF_OUT, 10, false),
         };
-        layers[0].A = X;
-        vector<pair<Matrix, Matrix>> grads(layers.size()); // (dW, db)
+        layers[0].A=X;
+        vector<pair<Matrix,Matrix>> grads(layers.size()); // (dW, db)
 
-        double a = 0.0005;
-        double cost = 1e10;
-        int epoch = 0;
-        while (cost > 0.1) {
+        double a=0.0005;
+        double cost=1e10;
+        int epoch=0;
+        while (cost>0.1) {
             ++epoch;
-            for (int i = 1; i < layers.size(); ++i)
-                forward_prop(layers[i], layers[i - 1]);
+            for (int i=1; i<layers.size(); ++i)
+                forward_prop(layers[i],layers[i-1]);
 
-            for (int i = layers.size() - 1; i > 0; --i) {
+            for (int i=layers.size()-1; i>0; --i) {
                 back_prop(
-                    layers[i], layers[i - 1], i == layers.size() - 1 ? nullptr : &layers[i + 1],
+                    layers[i], layers[i-1], i==layers.size()-1 ? nullptr : &layers[i+1],
                     Y, grads[i].first, grads[i].second, Loss::BinaryCrossentropy
                 );
             }
 
-            for (int i = 1; i < grads.size(); ++i) {
-                layers[i].W = layers[i].W - grads[i].first * a;
-                layers[i].b = layers[i].b - grads[i].second * a;
+            for (int i=1; i<grads.size(); ++i) {
+                layers[i].W=layers[i].W-grads[i].first*a;
+                layers[i].b=layers[i].b-grads[i].second*a;
             }
 
-            cost = mse_cost(layers.back().A, Y);
+            cost=mse_cost(layers.back().A,Y);
             if (isnan(cost)) {
                 fprintf(stderr, "cost became nan");
                 exit(1);
             }
 
-            if (epoch % 1000 == 0) {
-                printf("epoch %d | cost: %f\r", epoch, cost);
+            if (epoch%1000==0) {
+                printf("epoch %d | cost: %f\r", epoch,cost);
                 fflush(stdout);
             }
         }
-        cout << '\n';
+        cout<<'\n';
 
         string model;
-        for (const auto &l : layers) {
-            model += l.save() + '\n';
+        for (const auto &l:layers) {
+            model+=l.save() + '\n';
         }
         ofstream ofs("model");
-        ofs << model;
+        ofs<<model;
     } else {
+        ////////// INFERENCE
         vector<string> layer_data;
         ifstream ifs("model");
         string buf;
@@ -89,19 +91,19 @@ int main(int argc, char **argv) {
             layer_data.emplace_back(buf);
         }
 
-        layers = {
+        layers={
             Layer(layer_data[0]),
             Layer(layer_data[1]),
             Layer(layer_data[2]),
         };
 
-        layers[0].A = X;
+        layers[0].A=X;
 
-        for (int i = 1; i < layers.size(); ++i)
-            forward_prop(layers[i], layers[i - 1]);
+        for (int i=1; i<layers.size(); ++i)
+            forward_prop(layers[i],layers[i-1]);
     }
 
-    for (int i = 0; i < M; ++i) {
-        cout << layers.back().A.at(0, i) << " VS " << Y.at(0, i) << '\n';
+    for (int i=0; i<M; ++i) {
+        cout << layers.back().A.at(0,i) << " VS " << Y.at(0,i) << '\n';
     }
 }
